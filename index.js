7300c5de217d9d737e3c9ef812f67f8620751ca0
Karen Berba <kberba@gmail.com>
2018-01-28 19:19:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,3 +4,5 @@ const currentUser = 'Karen';
 const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
 
 const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+const shortGreeting = '';
